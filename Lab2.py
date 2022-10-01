8c8b84aeef1d798a4f3a87d178d76937b476ed0e
Cfberrio <98917677+Cfberrio@users.noreply.github.com>
2022-10-01 13:09:05 -0500

    Create Lab2.py

---
+++ Lab2.py
@@ -0,0 +1,18 @@
+import folium
+import pandas as pd
+import numpy as np
+data = pd.read_csv("co.csv")
+data_capitals=["capital","lat","lng"]
+data_capitals = data.loc[data['capital'].isin(["primary","admin"])]
+print(data_capitals)
+data_city = data_capitals[["city","lat","lng"]]
+
+
+m = folium.Map(location=[10.963889,-74.796387], zoom_start=13)
+
+lista = data_city.values.tolist()
+for row in lista:
+    folium.Marker([row[1], row[2]], popup=row[0]).add_to(m)
+m.save("try.html")
+
+data_v = pd.read_csv("vuelos.csv")
