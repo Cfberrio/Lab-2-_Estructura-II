32123e0294c978950c5fe21b62b178672904d882
Cfberrio <98917677+Cfberrio@users.noreply.github.com>
2022-10-05 19:58:29 -0500

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,12 +1,13 @@
+import mpu
+from geopy import Point
+import geopy.distance
 import folium
 import pandas as pd
 import numpy as np
 data = pd.read_csv("co.csv")
 data_capitals=["capital","lat","lng"]
 data_capitals = data.loc[data['capital'].isin(["primary","admin"])]
-print(data_capitals)
 data_city = data_capitals[["city","lat","lng"]]
-
 
 m = folium.Map(location=[10.963889,-74.796387], zoom_start=13)
 
@@ -15,4 +16,9 @@ for row in lista:
     folium.Marker([row[1], row[2]], popup=row[0]).add_to(m)
 m.save("try.html")
 
-data_v = pd.read_csv("vuelos.csv")
+
+distance_dt = pd.read_csv("distancia_v.csv")
+print(distance_dt.index)
+for index, row in distance_dt.iterrows():
+    lista= [mpu.haversine_distance((distance_dt["Latitud origen   "], distance_dt["Longitud origen "]), (distance_dt["latitud destino "], distance_dt["longitud destino"]))]
+
