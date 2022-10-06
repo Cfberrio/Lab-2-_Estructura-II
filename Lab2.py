ba63ebfd2a2e4c59cfe4a2a3466418cd8ae8d33d
unknown <dfsierra@uninorte.edu.co>
2022-10-06 01:28:55 -0500

    Cambie la forma de sacar distancias

--- Lab2.py
+++ Lab2.py
@@ -17,15 +17,18 @@ lista = data_city.values.tolist()
 for row in lista:
     folium.Marker([row[1], row[2]], popup=row[0]).add_to(m)
 m.save("try.html")
+#Creamos una deque para meter las distancias
 distancias = collections.deque()
 
-
+#Abrimos el .csv con pandas
 distance_dt = pd.read_csv("distancia_v.csv")
 print(distance_dt.index)
 
+#Iteramos entre las filas de las columnas especificadas para guardar las dos locaciones y poder usar haversine para conseguir la distancia
 for i in range(len(distance_dt)):
     loc1=(distance_dt.iloc[i]["Latitud origen   "], distance_dt.iloc[i]["Longitud origen "])
     loc2=(distance_dt.iloc[i]["latitud destino "], distance_dt.iloc[i]["longitud destino"])
+    #Metemos la distancia en la deque
     distancias.append(hs.haversine(loc1,loc2))
 
 print(distancias)
