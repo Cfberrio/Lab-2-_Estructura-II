653848aad80d8128adc434920ddc1c38404769f7
unknown <dfsierra@uninorte.edu.co>
2022-10-06 01:23:23 -0500

    No me servia lo de sacar las distancias entonces lo cambie.
    Meti todas las distancias en una lista con haversine.
    
    PD: Debemos organizar los archivos para que el datalab tenga las mismas ciudades y posiciones que el distancia_v y poder meter las distancias en el datalab.

--- Lab2.py
+++ Lab2.py
@@ -1,9 +1,11 @@
+import collections
 import mpu
 from geopy import Point
 import geopy.distance
 import folium
 import pandas as pd
 import numpy as np
+import haversine as hs
 data = pd.read_csv("co.csv")
 data_capitals=["capital","lat","lng"]
 data_capitals = data.loc[data['capital'].isin(["primary","admin"])]
@@ -15,10 +17,15 @@ lista = data_city.values.tolist()
 for row in lista:
     folium.Marker([row[1], row[2]], popup=row[0]).add_to(m)
 m.save("try.html")
+distancias = collections.deque()
 
 
 distance_dt = pd.read_csv("distancia_v.csv")
 print(distance_dt.index)
-for index, row in distance_dt.iterrows():
-    lista= [mpu.haversine_distance((distance_dt["Latitud origen   "], distance_dt["Longitud origen "]), (distance_dt["latitud destino "], distance_dt["longitud destino"]))]
 
+for i in range(len(distance_dt)):
+    loc1=(distance_dt.iloc[i]["Latitud origen   "], distance_dt.iloc[i]["Longitud origen "])
+    loc2=(distance_dt.iloc[i]["latitud destino "], distance_dt.iloc[i]["longitud destino"])
+    distancias.append(hs.haversine(loc1,loc2))
+
+print(distancias)
