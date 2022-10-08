706e88db7f3a62f2ff387f92b9ba5bd03223cb1b
Cfberrio <98917677+Cfberrio@users.noreply.github.com>
2022-10-08 17:33:43 -0500

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,34 +1,33 @@
-import collections
+import sys
 import mpu
 from geopy import Point
 import geopy.distance
 import folium
 import pandas as pd
 import numpy as np
-import haversine as hs
+print(sys.setrecursionlimit(4000))
 data = pd.read_csv("co.csv")
 data_capitals=["capital","lat","lng"]
 data_capitals = data.loc[data['capital'].isin(["primary","admin"])]
 data_city = data_capitals[["city","lat","lng"]]
 
 m = folium.Map(location=[10.963889,-74.796387], zoom_start=13)
-
 lista = data_city.values.tolist()
 for row in lista:
     folium.Marker([row[1], row[2]], popup=row[0]).add_to(m)
 m.save("try.html")
-#Creamos una deque para meter las distancias
-distancias = collections.deque()
 
-#Abrimos el .csv con pandas
-distance_dt = pd.read_csv("distancia_v.csv")
-print(distance_dt.index)
-
-#Iteramos entre las filas de las columnas especificadas para guardar las dos locaciones y poder usar haversine para conseguir la distancia
-for i in range(len(distance_dt)):
-    loc1=(distance_dt.iloc[i]["Latitud origen   "], distance_dt.iloc[i]["Longitud origen "])
-    loc2=(distance_dt.iloc[i]["latitud destino "], distance_dt.iloc[i]["longitud destino"])
-    #Metemos la distancia en la deque
-    distancias.append(hs.haversine(loc1,loc2))
-
-print(distancias)
+data_cords = pd.read_csv("datalab (3).csv")
+lista_cords_o= data_cords["Coordenadas.O"].values.tolist()
+lista_cords_d = data_cords["Coordenadas D"].values.tolist()
+cord1=[]
+points = []
+lista_cords_o.pop()
+lista_cords_o.pop()
+for i in range(len(lista_cords_o)):
+    cord1=lista_cords_o[i].split(', ')
+    lat1 =float(cord1[0])
+    lon1 =float(cord1[1])
+    points.append([cord1[0], cord1[1]])
+folium.PolyLine(points, color = "red").add_to(m)
+m.save("try.html")
